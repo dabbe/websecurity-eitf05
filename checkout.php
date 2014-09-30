7942ef4530d18a000fc0e2f0f80b51ff4876ce65
dabbe <fipaso@gmail.com>
2014-09-30 13:11:48 +0200

    Extracted css and fixed nice checkout.php page

--- checkout.php
+++ checkout.php
@@ -1,3 +1,10 @@
 <?php
+require_once("shoppingcart.php");
+$shopping_cart = new Shopping_Cart();
+$list = $shopping_cart->getList();
+
+echo '<link rel="stylesheet" href="style.css"/>';
+
+
 echo("You tried to buy your items.");
 ?>
