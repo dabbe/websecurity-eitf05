5ec78a3e5f4d5d5df90e03bbfdb2abcebe7294ec
Tomas <hawry@hawry.net>
2014-09-30 13:47:39 +0200

    Mer hal

--- test.php
+++ test.php
@@ -14,7 +14,7 @@ if (strcmp($_REQUEST['btn-login'],"Login")==0) {
 	echo "Results: ".$login;
 
 	if ($login) {
-		$_SESSION['username'] = $login['email'];
+		$_SESSION['username'] = $login[0]['email'];
 	}
 	$db->closeConnection();
 	Header("Location: index.php");
