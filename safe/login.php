4226ffcaa9880ba90a250484745db763b997ec89
Hawry <hawry@hawry.net>
2014-10-07 15:53:12 +0200

    Added safe login.php

---
+++ safe/login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+require_once('database.php');
+
+$db = new Database();
+$db->openConnection();
+
+if (strcmp($_POST['btn-login'],"Login")==0) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$login = $db->isValidLogin($username,$password);
+	if ($login) {
+		$_SESSION['username'] = $login[0]['email'];
+	}
+	$db->closeConnection();
+	Header("Location: index.php");
+	die();
+}
+
+if (strcmp($_POST['btn-register'],"Register")==0) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$results = $db->createUser($username,$password);
+	if ($results) {
+		$message = "User ".$username." is created";
+	}else{
+		$message = "That user already exists, please choose another username";
+	}
+	$db->closeConnection();
+	Header("Location: index.php?m=".$message);
+	die();
+}
+
+$db->closeConnection();
+Header("Location: index.php");
+die();
+?>
